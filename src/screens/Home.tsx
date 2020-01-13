5c380861c61dd85b2de6a5736fa5b14cb85e653b
Ageu Rodrigues <ageurdo@gmail.com>
2020-01-12 23:46:46 -0300

    Ref #001 First commit, only splash screen and buttons ready.
    Ref #001 First commit, only splash screen and buttons ready.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -10,20 +10,28 @@ export default function Home(){
             alignItems: 'center'
         },
         backgroundImage: {
-            width: '100%',
-            height: '100%',
+            flex: 1,
         },
         buttonAudio: {
             backgroundColor: '#f00',
-            margin: 10,
-            padding: 20,
-            borderRadius: 40,
+            margin: 20,
+            padding: 30,
+            borderRadius: 50,
             
             // flex: 1,
             // alignItems: 'center',
             // flexDirection: 'column',
             // justifyContent: 'center',
             // alignContent: 'center',
+
+        },
+        textButton: {
+            color: '#fff',
+            fontWeight: '700',
+            textTransform: 'uppercase'
+        },
+
+        imagesBackground:{
 
         },
     });
@@ -35,18 +43,21 @@ export default function Home(){
     //         <Text>teste</Text>
     //     </View>
     // </ImageBackground>
+        <ImageBackground style = {styles.backgroundImage} source={require('./../../assets/splash.png')}>
+        <View style={styles.container}>
+            <TouchableOpacity style={[styles.buttonAudio]}>
+                <Text style={styles.textButton}>Cinto de Segurança</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonAudio}>
+                <Text style={styles.textButton}>Não esqueça seus pertences</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonAudio}>
+                <Text style={styles.textButton}>Boa viagem</Text>
+            </TouchableOpacity>
+        </View>
+        </ImageBackground>
+    
 
-    <View style={styles.container}>
-        <TouchableOpacity style={styles.buttonAudio}>
-            <Text>Cinto de Segurança</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.buttonAudio}>
-            <Text>Cinto de Segurança</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.buttonAudio}>
-            <Text>Cinto de Segurança</Text>
-        </TouchableOpacity>
-    </View>
     );
 
   
